1280a7ff0d0e6c820161a7ebfbef3b53f0698ac0
Young-097 <vosejgo@gmail.com>
2025-07-17 13:40:39 +0900

    Revert "Mission 2"

--- src/MovieDetail.jsx
+++ src/MovieDetail.jsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react";
 import "./css/MovieDetail.css"
 import movieListData from"./data/movieListData.json"
 import { useParams } from 'react-router-dom'
 
 const baseUrl = "https://image.tmdb.org/t/p/w500"
 
-const key = import.meta.env.VITE_TMDB_API_KEY;
-
 function MovieDetail () {
-    const {id} = useParams()
-    const [movie, setMovie] = useState(null)
+    const params = useParams()
+    console.log(params.id)
 
-    useEffect(()=>{
-              fetch(`https://api.themoviedb.org/3/movie/${id}?language=ko-KR`,{
-                  headers: {
-                      accept: "application/json",
-                      Authorization: `Bearer ${key}`
-                  },
-              })
-              .then((res)=>res.json())
-              .then((data)=>{
-                  setMovie(data)
-              })
-              .catch((err)=>{
-                  console.error("영화 상세 정보 요청 실패", err)
-              })
-          },[id])
-
-          if(!movie) return <div>로딩 중 ...</div>;  //????
+    const movie = movieListData.results.find((el)=>el.id === Number(params.id))
+    console.log(movie)
     return(
         <>
           <div>
@@ -36,15 +18,16 @@ function MovieDetail () {
                     <img className="Detail-img" src={baseUrl + movie.poster_path}/>
                 </div>
                 <div className="Detail-detail">
-                
-                    <div className="Detail-title">
-                        {movie.title}
-                    </div>
-                    <div className="Detail-average">
-                        평점 : {movie.vote_average}
+                    <div className="Detail-title-average">
+                        <div className="Detail-title">
+                            {movie.title}
+                        </div>
+                        <div className="Detail-average">
+                            평점 : {movie.vote_average}
+                        </div>
                     </div>
                     <div className="Detail-genre">
-                        {movie.genres.map((genre)=> genre.name).join(" , ")}
+                        장르 보류
                     </div>
                     <div className="Detail-overviewd">줄거리</div>
                     <div className="Detail-overview">
